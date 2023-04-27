d8d8c6fe4a82c29fb9fd74b5d12b229a3daa6c26
Taylor Smith <smith.taylord@gmail.com>
2023-04-27 08:10:48 -0600

    event cards are looking good!

--- TowerReactDotNet.client/src/components/EventCard.jsx
+++ TowerReactDotNet.client/src/components/EventCard.jsx
@@ -1,21 +1,43 @@
 import React from 'react';
+import '../assets/scss/components/EventCard.scss'
 import PropTypes from 'prop-types'
 
 export default function EventCard({event}) {
 
     return (
-
-        <>
-        <img 
-        className='sm-event-pic'
-        src={event.coverImg} 
-        alt={event.name} />
-        <div>
-            <h3>{event.name}</h3>
-            <div>{event.capacity} Spots Left</div>
+        <div className='event-card tower-box-shadow card-border rounded'>
+            <div className='card position-relative card-border rounded'>
+                <img 
+                className='img-fluid event-img selectable'
+                src={event.coverImg} 
+                alt={event.name} />
+                <div className='position-absolute bottom-0 start-0 glass-card rounded-bottom'>
+                    <div className='ps-2 pt-3 text-body-bg'>
+                            <b>{event.name}</b>
+                    </div>
+                    <div className='ps-2 text-info'>{event.location}</div>
+                    <div className='px-2 pb-1 text-info text-end'> 
+                        <b className='text-primary pe-2'>{event.capacity}</b>
+                        spots left
+                    </div>
+                </div>
+            </div>
         </div>
-        </>
+
+
+
+        // <>
+        // <img 
+        // className='event-img'
+        // src={event.coverImg} 
+        // alt={event.name} />
+        // <div>
+        //     <h3>{event.name}</h3>
+        //     <div>{event.capacity} Spots Left</div>
+        // </div>
+        // </>
     )
+
 
 }
 
