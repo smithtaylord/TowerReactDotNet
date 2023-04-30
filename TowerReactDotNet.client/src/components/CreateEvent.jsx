23686a3dfac9ce513285be66a3d845b4343ece83
Taylor Smith <smith.taylord@gmail.com>
2023-04-30 10:01:27 -0600

    Form does not function, but it looks good!

--- TowerReactDotNet.client/src/components/CreateEvent.jsx
+++ TowerReactDotNet.client/src/components/CreateEvent.jsx
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function CreateEvent() {
+    const [selectedValue, setSelectedValue] = useState('');
 
+    const handleSelectChange = (event) => {
+        setSelectedValue(event.target.value);
+    };
     return (
 
 
         <div className="modal-content bg-dark">
-            <div className="modal-header">
-                <h1 className="modal-title fs-5" id="createEventModalLabel">Create Event</h1>
-                <div className="selectable text-light fs-3" data-bs-dismiss="modal" aria-label="Close">&times;</div>
+            <div className="bg-secondary text-info rounded-top p-3 d-flex justify-content-between mb-3">
+                <div> </div>
+                <h1 className="modal-title fs-3" id="createEventModalLabel">Create Event</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div className="modal-body bg-dark">
+            <div className="modal-body bg-dark px-4">
                 <form>
                     <div className="row">
                         <div className="col-6">
@@ -53,11 +58,15 @@ export default function CreateEvent() {
                             </div>
                         </div>
                         <div className="col-6">
-                            <select className="selectbox" aria-label="Default select example">
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
-                            </select>
+                            <div className={`select-container ${selectedValue != '' ? 'selected' : ''}`}>
+                                <select id="event-type-select" className="selectbox" aria-label="Event Type" value={selectedValue} onChange={handleSelectChange}>
+                                    <option value="" disabled>Event Type</option>
+                                    <option value="1">One</option>
+                                    <option value="2">Two</option>
+                                    <option value="3">Three</option>
+                                </select>
+                                {/* <label className={selectedValue == '' ? 'd-none' : ''}>Event Type</label> */}
+                            </div>
                         </div>
                     </div>
                     <div className="row">
@@ -76,9 +85,9 @@ export default function CreateEvent() {
 
 
             </div>
-            <div className="modal-footer">
-                <button type="button" className="selectable btn bg-secondary" data-bs-dismiss="modal">Close</button>
-                <button type="button" className="selectable btn bg-success">Save changes</button>
+            <div className="px-3 pb-4 text-end">
+                {/* <button type="button" className="selectable btn bg-secondary me-3" data-bs-dismiss="modal">Close</button> */}
+                <button type="button" className="selectable btn bg-success me-3 tower-box-shadow">Save changes</button>
             </div>
         </div>
 
