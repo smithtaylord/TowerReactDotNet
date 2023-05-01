c08c23de529c470405d297d6b253e8ec8b88182b
Taylor Smith <smith.taylord@gmail.com>
2023-05-01 10:27:53 -0600

    working with bcw  form handler

--- TowerReactDotNet.client/src/components/CreateEvent.jsx
+++ TowerReactDotNet.client/src/components/CreateEvent.jsx
@@ -1,11 +1,22 @@
 import React, { useState } from 'react';
+import { BindEditable } from '../utils/FormHandler.js';
+import Pop from '../utils/Pop.js';
+import { logger } from '../utils/Logger.js';
 
 export default function CreateEvent() {
-    const [selectedValue, setSelectedValue] = useState('');
+    const editable = ({ type: '' })
+    const bindEditable = BindEditable(editable)
 
-    const handleSelectChange = (event) => {
-        setSelectedValue(event.target.value);
-    };
+
+    async function handleSubmit() {
+        try {
+            window.event?.preventDefault()
+            logger.log({ editable })
+        }
+        catch (error) {
+            Pop.error(error);
+        }
+    }
     return (
 
 
@@ -15,19 +26,29 @@ export default function CreateEvent() {
                 <h1 className="modal-title fs-3" id="createEventModalLabel">Create Event</h1>
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div className="modal-body bg-dark px-4">
-                <form>
+            <form onSubmit={handleSubmit}>
+                <div className="modal-body bg-dark px-4">
                     <div className="row">
                         <div className="col-6">
                             <div className="inputbox w-100 mb-3">
-                                <input required type="text" />
+                                <input
+                                    required
+                                    type="text"
+                                    id='name'
+                                    name='name'
+                                    onChange={bindEditable} />
                                 <span>Name</span>
                                 <i></i>
                             </div>
                         </div>
                         <div className="col-6">
                             <div className="inputbox w-100 mb-3">
-                                <input required type="text" />
+                                <input
+                                    required
+                                    type="text"
+                                    id='coverImg'
+                                    name='coverImg'
+                                    onChange={bindEditable} />
                                 <span>Cover Image</span>
                                 <i></i>
                             </div>
@@ -36,14 +57,24 @@ export default function CreateEvent() {
                     <div className="row">
                         <div className="col-6">
                             <div className="inputbox w-100 mb-3">
-                                <input required type="text" />
+                                <input
+                                    required
+                                    type="text"
+                                    id='location'
+                                    name='location'
+                                    onChange={bindEditable} />
                                 <span>Location</span>
                                 <i></i>
                             </div>
                         </div>
                         <div className="col-6">
                             <div className="inputbox w-100 mb-3">
-                                <input required type="date" />
+                                <input
+                                    required
+                                    type="date"
+                                    id='startDate'
+                                    name='startDate'
+                                    onChange={bindEditable} />
                                 <span>Date</span>
                                 <i></i>
                             </div>
@@ -52,44 +83,56 @@ export default function CreateEvent() {
                     <div className="row">
                         <div className="col-6">
                             <div className="inputbox w-100 mb-3">
-                                <input required type="number" />
+                                <input
+                                    required
+                                    type="number"
+                                    id='capacity'
+                                    name='capacity'
+                                    onChange={bindEditable} />
                                 <span>Capacity</span>
                                 <i></i>
                             </div>
                         </div>
                         <div className="col-6">
-                            <div className={`select-container ${selectedValue != '' ? 'selected' : ''}`}>
-                                <select id="event-type-select" className="selectbox" aria-label="Event Type" value={selectedValue} onChange={handleSelectChange}>
+                            <div className={`select-container ${editable.type != '' ? 'selected' : ''}`}>
+                                <select
+                                    id="event-type-select"
+                                    className="selectbox"
+                                    aria-label="Event Type"
+                                    name='type'
+                                    defaultValue={editable.type}
+                                    onChange={bindEditable}>
                                     <option value="" disabled>Event Type</option>
-                                    <option value="1">One</option>
-                                    <option value="2">Two</option>
-                                    <option value="3">Three</option>
+                                    <option value="concert">Concert</option>
+                                    <option value="convention">Convention</option>
+                                    <option value="sport">Sport</option>
+                                    <option value="digital">Digital</option>
+                                    <option value="misc.">Mics.</option>
                                 </select>
-                                {/* <label className={selectedValue == '' ? 'd-none' : ''}>Event Type</label> */}
                             </div>
                         </div>
                     </div>
                     <div className="row">
                         <div className="col-12">
                             <div className="inputbox w-100 mb-3">
-                                <input required type="text" />
+                                <input
+                                    required
+                                    type="text"
+                                    id='description'
+                                    name='description'
+                                    onChange={bindEditable} />
                                 <span>Event Description</span>
                                 <i></i>
                             </div>
                         </div>
-
                     </div>
-
-
-                </form>
-
-
-            </div>
-            <div className="px-3 pb-4 text-end">
-                {/* <button type="button" className="selectable btn bg-secondary me-3" data-bs-dismiss="modal">Close</button> */}
-                <button type="button" className="selectable btn bg-success me-3 tower-box-shadow">Save changes</button>
-            </div>
-        </div>
+                </div>
+                <div className="px-3 pb-4 text-end">
+                    {/* <button type="button" className="selectable btn bg-secondary me-3" data-bs-dismiss="modal">Close</button> */}
+                    <button type="submit" className="selectable btn bg-success me-3 tower-box-shadow">Save changes</button>
+                </div>
+            </form>
+        </div >
 
     )
 
