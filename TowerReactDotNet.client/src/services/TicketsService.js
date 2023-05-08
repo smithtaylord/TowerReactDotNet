0b1427e6a6e80745e791f9f326eeceb79d038100
Taylor Smith <smith.taylord@gmail.com>
2023-05-08 17:57:43 -0600

    Capacity is rective

--- TowerReactDotNet.client/src/services/TicketsService.js
+++ TowerReactDotNet.client/src/services/TicketsService.js
@@ -1,3 +1,5 @@
+import { AppState } from "../AppState.js"
+import { TowerEvent } from "../models/TowerEvent.js"
 import { logger } from "../utils/Logger.js"
 import { api } from "./AxiosService.js"
 
@@ -6,6 +8,7 @@ class TicketsService{
 async createTicket(eventId){
     const res = await api.post('api/tickets', {eventId})
     logger.log('[create ticket]', res.data)
+    AppState.activeEvent = new TowerEvent(res.data.event)
 }
 
 }
