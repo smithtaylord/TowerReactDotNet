bc6a5d805b80e9cb5ded666e3073840712b6cab7
Taylor Smith <smith.taylord@gmail.com>
2023-05-22 05:43:46 -0600

    Made Loader file

---
+++ TowerReactDotNet.client/src/components/Loader.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Loader() {
+
+    return (
+
+        <div className="Loader">
+            <div className="center">
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+                <div className="wave"></div>
+            </div>
+        </div>
+    )
+
+}
