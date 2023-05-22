c33e6d8b924c049dad0ea16bff508cb6bf9368fd
Taylor Smith <smith.taylord@gmail.com>
2023-05-22 06:12:05 -0600

    Can get ticket elements to draw to page and there is a loader

--- TowerReactDotNet.client/src/pages/AccountPage.jsx
+++ TowerReactDotNet.client/src/pages/AccountPage.jsx
@@ -1,22 +1,31 @@
 import { observer } from "mobx-react";
 import React from "react";
 import { AppState } from "../AppState.js";
+import Loader from '../components/Loader.jsx'
 
 function AccountPage() {
 
-  return (
-    <div className="account-page">
-      <div className="card">
-        <div className="card-body p-5 text-center">
-          <img src={AppState.account.picture}
-            alt={AppState.account.name}
-            className="rounded-circle" height="200" />
-          <p className="display-6 my-2">{AppState.account.name}</p>
-          <kbd>
-            {AppState.account.email}
-          </kbd>
+  let tickets = AppState.myTickets.map(t => {
+    return (
+      <div key={t.id} className="container-fluid">
+        <div className="row">
+          <div className="col-3 ">
+            <img
+              src={t.event.coverImg}
+              alt={t.event.name}
+              className="ticket-pic img-fluid" />
+          </div>
+          <div className="col-9"></div>
         </div>
       </div>
+    )
+  })
+
+  return (
+    <div className="account-page">
+      <h1 className="text-center text-info border-bottom py-4 bg-dark bg-gradient">Welcome, {AppState.account?.name}</h1>
+      <h3 className="p-3">Tickets</h3>
+      {AppState.myTickets.length == 0 ? <Loader /> : tickets}
     </div>
   )
 }
