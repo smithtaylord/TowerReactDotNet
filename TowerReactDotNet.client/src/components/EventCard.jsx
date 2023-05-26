a532582874fd53be5511778bda4b2afac0936af3
Taylor Smith <smith.taylord@gmail.com>
2023-05-26 13:44:08 -0600

    added event canceled banner

--- TowerReactDotNet.client/src/components/EventCard.jsx
+++ TowerReactDotNet.client/src/components/EventCard.jsx
@@ -6,7 +6,17 @@ import { Link } from 'react-router-dom';
 export default function EventCard({ event }) {
     const zeroSpots = <b className='text-danger pe-2'>0</b>
     const spots = <b className='text-primary pe-2'>{event.capacity}</b>
-    const eventCanceled = <b className='text-primary pe-2'>EVENT CANCELED</b>
+    const eventCanceled = (
+        <div className='sold-out tower-box-shadow'>
+            <span className='p-3'>Canceled</span>
+        </div>
+    )
+
+    const soldOut = (
+        <div className='sold-out tower-box-shadow'>
+            <span className='p-3'>SOLD OUT</span>
+        </div>
+    )
 
 
     return (
@@ -19,17 +29,16 @@ export default function EventCard({ event }) {
                         src={event.coverImg}
                         alt={event.name} />
                 </Link>
-                <div className='sold-out tower-box-shadow'>
-                    <span className='p-3'>SOLD OUT</span>
-                </div>
+                {event.capacity <= 0 && !event.isCanceled ? soldOut : null}
+                {event.isCanceled ? eventCanceled : null}
                 <div className='position-absolute bottom-0 start-0 glass-card rounded-bottom'>
                     <div className='ps-2 pt-3 text-body-bg'>
                         <b>{event.name}</b>
                     </div>
                     <div className='ps-2 text-info'>{event.location}</div>
                     <div className='px-2 pb-1 text-info text-end'>
-                        {event.isCanceled ? eventCanceled : event.capacity <= 0 ? zeroSpots : spots}
-                        {event.isCanceled ? '' : 'Spots Left'}
+                        {event.capacity <= 0 ? zeroSpots : spots}
+                        Spots Left
                     </div>
                 </div>
             </div>
