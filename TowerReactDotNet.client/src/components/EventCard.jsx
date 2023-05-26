6320e4e8bcf9326e19a7d5b399e21a3d6983fc75
Taylor Smith <smith.taylord@gmail.com>
2023-05-26 13:49:35 -0600

    Hover Effect added

--- TowerReactDotNet.client/src/components/EventCard.jsx
+++ TowerReactDotNet.client/src/components/EventCard.jsx
@@ -1,33 +1,34 @@
 import React from 'react';
-import '../assets/scss/components/EventCard.scss'
-import PropTypes from 'prop-types'
+import '../assets/scss/components/EventCard.scss';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export default function EventCard({ event }) {
-    const zeroSpots = <b className='text-danger pe-2'>0</b>
-    const spots = <b className='text-primary pe-2'>{event.capacity}</b>
+    const zeroSpots = <b className='text-danger pe-2'>0</b>;
+    const spots = <b className='text-primary pe-2'>{event.capacity}</b>;
     const eventCanceled = (
         <div className='sold-out tower-box-shadow'>
             <span className='p-3'>Canceled</span>
         </div>
-    )
-
+    );
     const soldOut = (
         <div className='sold-out tower-box-shadow'>
             <span className='p-3'>SOLD OUT</span>
         </div>
-    )
-
+    );
 
     return (
-        <div style={event.isCanceled ? { filter: 'grayscale(100%)' } : {}}
-            className='tower-box-shadow card-border rounded'>
+        <div
+            style={event.isCanceled ? { filter: 'grayscale(100%)' } : {}}
+            className='event-card tower-box-shadow card-border rounded'
+        >
             <div className='position-relative card-border rounded'>
                 <Link to={`event/${event.id}`}>
                     <img
                         className='img-fluid event-img selectable'
                         src={event.coverImg}
-                        alt={event.name} />
+                        alt={event.name}
+                    />
                 </Link>
                 {event.capacity <= 0 && !event.isCanceled ? soldOut : null}
                 {event.isCanceled ? eventCanceled : null}
@@ -43,11 +44,9 @@ export default function EventCard({ event }) {
                 </div>
             </div>
         </div>
-    )
-
-
+    );
 }
 
 EventCard.propTypes = {
-    event: PropTypes.object.isRequired
-}
+    event: PropTypes.object.isRequired,
+};
