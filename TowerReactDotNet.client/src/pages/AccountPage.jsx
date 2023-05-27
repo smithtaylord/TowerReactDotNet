68246223fd37ccd06267c979e39f242b97002c55
Taylor Smith <smith.taylord@gmail.com>
2023-05-27 08:27:13 -0600

    Ticket Pages Looks good on mobile :)

--- TowerReactDotNet.client/src/pages/AccountPage.jsx
+++ TowerReactDotNet.client/src/pages/AccountPage.jsx
@@ -31,33 +31,33 @@ function AccountPage() {
 
   let tickets = AppState.myTickets.map(t => {
     return (
-      <div key={t.id} className="container mb-5 tower-box-shadow rounded bg-gradient">
-        <div className="row">
-          <div className="col-3 p-0 ">
-            <Link to={`/event/${t.event.id}`}>
-              <img
-                src={t.event.coverImg}
-                alt={t.event.name}
-                className="ticket-pic img-fluid rounded-start" />
-            </Link>
-          </div>
-          <div className="col-9">
-            <div className="d-flex flex-column">
-              <div className="p-4">
-                <div className="text-white fs-4 fw-bold">{t.event.name}</div>
-                <div className="text-info fs-6">{t.event.location}</div>
-                <div className="text-info fs-6">{t.event.startDate}</div>
-              </div>
-              <div className="text-end px-4">
-                <button onClick={() => handleClick(t.id, t.event.id)}
-                  className="btn bg-danger tower-box-shadow selectable">
-                  Not Going
-                </button>
-              </div>
+
+      <div key={t.id} className="row mb-2 mb-md-5 tower-box-shadow rounded bg-gradient">
+        <div className="col-3 p-0 ">
+          <Link to={`/event/${t.event.id}`}>
+            <img
+              src={t.event.coverImg}
+              alt={t.event.name}
+              className="ticket-pic img-fluid rounded-start" />
+          </Link>
+        </div>
+        <div className="col-9">
+          <div className="d-flex flex-column">
+            <div className="p-2 p-md-4">
+              <div className="text-white fs-4 fw-bold">{t.event.name}</div>
+              <div className="text-info fs-6">{t.event.location}</div>
+              <div className="text-info fs-6">{t.event.startDate}</div>
+            </div>
+            <div className="text-end px-4">
+              <button onClick={() => handleClick(t.id, t.event.id)}
+                className="btn bg-danger tower-box-shadow selectable">
+                Not Going
+              </button>
             </div>
           </div>
         </div>
       </div>
+
     )
   })
 
@@ -68,8 +68,14 @@ function AccountPage() {
   return (
     <div className="account-page">
       <h1 className="text-center text-info border-bottom py-4 bg-dark bg-gradient">{possive} Tickets</h1>
-      <div className="p-5 m-5">
-        {AppState.myTickets.length == 0 ? <Loader /> : tickets}
+      <div className="p-md-5 m-md-5">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-11 col-lg-9 m-auto">
+              {AppState.myTickets.length == 0 ? <Loader /> : tickets}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
