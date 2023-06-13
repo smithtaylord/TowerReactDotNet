5f91036af8ad2187ac3b42156098ad9f478e09b2
Taylor Smith <smith.taylord@gmail.com>
2023-06-13 09:49:10 -0600

    building for production

--- TowerReactDotNet.client/vite.config.js
+++ TowerReactDotNet.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'docs',
+    outDir: '../TowerReactDotNet/wwwroot',
     sourcemap: false
   },
   server: {
